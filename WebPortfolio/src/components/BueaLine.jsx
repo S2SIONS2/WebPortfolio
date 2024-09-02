6ee6f6bc7455f0cf9423e30bdd176f6d9b79fce9
Sion <solee9802@gmail.com>
2024-09-02 20:23:43 +0900

    뷰라인 추가 설명 커밋 및 about 자기 소래 수정

--- WebPortfolio/src/components/BueaLine.jsx
+++ WebPortfolio/src/components/BueaLine.jsx
@@ -7,6 +7,7 @@ import { faX } from '@fortawesome/free-solid-svg-icons'
 import bueaLine from '../assets/bueaLine.png'
 import bueaCategory from '../assets/bueaCategory.png';
 import bueaCategory2 from '../assets/bueaCategory2.png';
+import bueaLine2 from '../assets/bueaLine2.png'
 
 const BueaLine = ({modal2, setModal2}) => {
     return(
@@ -39,39 +40,57 @@ const BueaLine = ({modal2, setModal2}) => {
                     <img src={bueaLine} alt="" />
                     <img src={bueaCategory} alt="" />
                     <img src={bueaCategory2} alt="" />
+                    <img src={bueaLine2} alt="" />
                 </section>
                 <section className='projectIntro'>
                     <ul>
                         <li>소개: 가게 운영을 위한 모바일 관리자 웹앱입니다.</li>
                         <li>개발 인원: 2인</li>
                         <li>담당 역할: 기획, 프론트 개발, 퍼블리싱, 디자인</li>
-                        <li>사용 기술: React, Bootstrap5, Scss, JWT, API</li>
+                        <li>사용 기술: React, Bootstrap5, Scss, JWT, API, Recharts</li>
                         <li>로그인 - ID: user1, PW: 1111</li>
                     </ul>
                 </section>
                 <section className='projectExplain'>
-                    <div className='subTitle'>주요 기능</div>
+                    <div className='subTitle'>주요 기능 - api 제작 등 백 제외 프론트 작업 100% 진행</div>
                     <ul>
                         <li>로그인 기능 구현 및 로그인 유지</li>
+                            <span>- JWT 기반 로그인 구현</span>
                         <li>API를 이용하여 리스트 호출, 추가, 수정 및 삭제와 검색기능</li>
-                        <li>연락처 관리를 위한 전화번호부 페이지</li>
-                        <li>총 작업 내역 리스트 - 작업 추가 등 현재 개발중</li>
+                            <span>- 연락처 관리를 위한 전화번호부 페이지</span>
+                            <span>- 작업 종류 관리를 위한 카테고리 페이지</span>
+                            <span>- 총 작업 내역 리스트</span>
+                        <li>총 작업 내역 리스트</li>
+                            <span>- 작업(리스트) 등록, 수정, 삭제 가능</span>
+                            <span>- 날짜, 이름 별 검색 가능</span>
+                            <span>- 미수금 내역 체크 시 외상 리스트만 확인</span>
+                            <span>- 작업 완료 버튼과 리스트 체크박스로 작업 일괄 처리 가능</span>
+                        <li>통계 차트</li>
+                            <span>- Recharts 라이브러리를 이용한 차트 작업</span>
+                            <span>- 총 예상 결과, 시술 별 총 금액, 총 인원 등의 차트 표시</span>
+                            <span>- 바 차트, 원형 차트 두가지 차트 종류</span>
                     </ul>           
                 </section>
                 <section className='projectExplain'>
                     <div className='subTitle'>추가 예정 기능</div>
                     <ul>
-                        <li>진행 중인 작업 내역 리스트</li>
-                        <li>통계 차트</li>
+                        <li>작업 내역 리스트 수정 중 작업 추가 가능하게 수정 예정</li>
+                        <li>전반적인 기능 추가 후 apk, 앱으로 제작 예정</li>
                     </ul>
                 </section>
                 <section className='projectTroubleShooting'>
                     <div className='subTitle'>트러블 슈팅</div>
                     <div className='troubleIssue'>1. 로그인 연결 후 리스트 추가 오류</div>
+                    <p>- 문제 상황: 헤더에 액세스 토큰 값이 없어 화면이 뜨지 않고 오류가 발생</p>
                     <p>- 해결 방법: api호출 시 헤더에 액세스 토큰 값으로 권한을 부여한다.</p>
-                    <div className='troubleIssue'>2. 리스트 호출 오류</div>
+                    <div className='troubleIssue'>2. 비동기 처리로 인한 api 호출 오류</div>
+                    <p>- 문제 상황: 새로고침 시에는 list 값이 담기지 않고, 화면 리렌더링시 list 값이 담김.</p>
+                    <p>- 해결 방법: useEffect를 이용, list 값이 변할 때 다시 리렌더링하며 화면에 표현함</p>
+                    <p>- 블로그 포스팅: <a href='https://deer-develope-diary.tistory.com/26' target='_blank'>https://deer-develope-diary.tistory.com/26</a></p>
+                    <div className='troubleIssue'>3. 리스트 호출 오류</div>
                     <p>- 문제 상황: 리스트 호출이 계속 반복해서 일어나 검색, 분류 기능이 작동했다가 안됐다가 반복함.</p>
-                    <p>- 해결 방법: useEffect로 반복 호출되던 리스트 호출 함수를 정리 및 getList 함수로 api 호출 통일화 </p>
+                    <p>- 해결 방법: useEffect로 반복 호출 되어 야기되던 에러를 해결하기 위해 리스트 호출 함수를 정리 및 getList 함수로 api 호출 통일화 </p>
+                    <p>- 알게된 점: 무분별한 useEffect 사용은 오히려 실행 순서에 대한 혼란스러움을 가중시킬 수 있으니, 상황과 순서에 맞게 작성하기</p>
                 </section>
                 <section className='projectAfter'>
                     <div className='subTitle'>프로젝트를 통해 배운점</div>
@@ -84,6 +103,7 @@ const BueaLine = ({modal2, setModal2}) => {
                         <div className='troubleIssue'>2. API get과 post 차이</div>
                         <p>- get: api 리스트를 불러올 때 사용하며, 파라미터에 특정 조건을 넣으면 조건에 맞는 리스트만 불러올 수 있다.</p>
                         <p>- post: api 리스트를 추가, 수정, 삭제 등 서버에 리스트(데이터)를 보내 리스트를 변경 할 수 있다.</p>
+                        <p>- 블로그 포스팅: 1. APi get, post 차이 설명 <a href='https://deer-develope-diary.tistory.com/25' target='_blank'>https://deer-develope-diary.tistory.com/25</a></p>
                     </div>
                 </section>
             </div>
--- WebPortfolio/src/pages/About.jsx
+++ WebPortfolio/src/pages/About.jsx
@@ -19,8 +19,13 @@ const About = () => {
                 </div>
                 <div className="intro">
                     <h1>이시온</h1>
-                    <p>1998. 02. 12</p>
-                    <p>안산대학교 식품영양과 졸업 후 방송통신대학교 식품영양학과를 졸업하였습니다.</p>
+                    <p className='mb-1'>1998. 02. 12</p>
+                    <p className='mb-1'>안산대학교 식품영양과 졸업 후 방송통신대학교 식품영양학과를 졸업하였습니다.</p>
+                    <p className='mb-1'><span className='lightPink'>새로운 것을 배우고 실행 하는 것을 좋아하며,</span> 
+                        퍼블리셔 근무 당시 <span className='border-bottom'>최신 css인 container 코드를 사용하여 스크립트 코드를 간략화</span> 
+                        한 적이 있습니다. 
+                    </p>
+                    <p>회사 내 유일한 퍼블리셔로 근무하며 <span className='lightPink'>웹 표준화와 웹 접근성을 지향한 코드를 작성</span>했으며, 적응력이 좋습니다. </p>
                 </div>
             </section>
             <section className="career">
@@ -29,10 +34,29 @@ const About = () => {
                     <div className="box">
                         <ul>
                             <li>
-                                <h2>(주) 텍트원 [2022.06 ~ 2024.03]</h2>
+                                <h2>(주) 텍트원 [2022.06 ~ 2024.03] - 퍼블리셔</h2>
                             </li>
                             <li>
                                 <h2>1. 스마트 팩토리 기반 웹페이지 퍼블리싱</h2>
+                            </li>
+                            <li>
+                                <span className='ms-1'>1.1. [LG PraiCam]</span>
+                            </li>
+                            <li>
+                                <p> - HTML, CSS, jQuery를 이용한 화면 퍼블리싱</p>                                                             
+                            </li>
+                            <li>
+                                <p> - 최신 CSS 문법을 사용하여 스크립트 최소화</p>
+                            </li>
+                            <li>
+                                <p> - 반응형 웹사이트 및 크로스 브라우징 구현</p>
+                            </li>
+                            <li className='mb-1'>
+                                <p> - 객체 사이즈에 따라 달라지는 화면 알람 구현</p>
+                            </li>
+
+                            <li>
+                                <span className='ms-1'>1.2. [LG PraiCam Cloud]</span>
                             </li>
                             <li>
                                 <p> - HTML, CSS, jQuery를 이용한 화면 퍼블리싱</p>                                                             
@@ -44,6 +68,59 @@ const About = () => {
                                 <p> - 반응형 웹사이트 및 크로스 브라우징 구현</p>
                             </li>
                             <li>
+                                <p> - 웹 표준화 및 웹 접근성 준수 </p>
+                            </li>
+                            <li className='mb-1'>
+                                <p> - 차트 및 그래프 퍼블리싱</p>
+                            </li>
+
+                            <li>
+                                <span className='ms-1'>1.3. [LG 양극재]</span>
+                            </li>
+                            <li>
+                                <p> - HTML, CSS, jQuery를 이용한 화면 퍼블리싱</p>                                                             
+                            </li>
+                            <li>
+                                <p> - 최신 CSS 문법을 사용하여 스크립트 최소화</p>
+                            </li>
+                            <li>
+                                <p> - 반응형 웹사이트 및 크로스 브라우징 구현</p>
+                            </li>
+                            <li>
+                                <p> - 이미지 누끼 제거 및 웹 화면 디자인</p>
+                            </li>
+                            <li>
+                                <p> - 도면 위 알람 영역 표시</p>
+                            </li>
+                            <li className='mb-1'>
+                                <p> - 차트 및 그래프 퍼블리싱</p>
+                            </li>
+
+                            <li>
+                                <span className='ms-1'>1.4. [그 외 기타 프로젝트]</span>
+                            </li>
+                            <li>
+                                <p> - 공장 대시보드 화면 퍼블리싱</p>
+                            </li>
+                            <li>
+                                <p> - 애니메이션을 이용한 알람 표시</p>
+                            </li>
+                            <li>
+                                <p> - HTML, CSS, jQuery를 이용한 화면 퍼블리싱</p>                                                             
+                            </li>
+                            <li>
+                                <p> - 최신 CSS 문법을 사용하여 스크립트 최소화</p>
+                            </li>
+                            <li>
+                                <p> - 반응형 웹사이트 및 크로스 브라우징 구현</p>
+                            </li>
+                            <li>
+                                <p> - 이미지 누끼 제거 및 웹 화면 디자인</p>
+                            </li>
+                            <li>
+                                <p> - 도면 수정 및 웹 디자인</p>
+                            </li>
+                            <li className='mb-1'>
                                 <p> - 차트 및 그래프 퍼블리싱</p>
                             </li>
                         </ul>
@@ -64,7 +141,7 @@ const About = () => {
             <section className='skills'>
             <div className="boxWrap">
                     <h1>능력</h1>
-                    <div className="box skillBox">
+                    <div className="box skillBox mb-1">
                         <ul>
                             <li>
                                 <h2>편해요: </h2>                
@@ -85,11 +162,14 @@ const About = () => {
                                 <p>
                                     <span><FontAwesomeIcon icon={faReact} /></span>React
                                 </p>
+                                <p>
+                                    <span><FontAwesomeIcon icon={faBootstrap} /></span>Bootstrap
+                                </p>
                                 <p>Svn</p>
                             </li>
                         </ul>
                     </div>
-                    <div className='box skillBox'>
+                    <div className='box skillBox mb-1'>
                         <ul>
                             <li>
                                 <h2>어느 정도 사용 할 수 있어요:</h2>                   
@@ -99,14 +179,87 @@ const About = () => {
                                     <span><FontAwesomeIcon icon={faSass} /></span>Scss
                                 </p>
                                 <p>
-                                    <span><FontAwesomeIcon icon={faBootstrap} /></span>Bootstrap
-                                </p>
-                                <p>
                                     <span><FontAwesomeIcon icon={faFigma} /></span>Figma
                                 </p>
                                 <p>
                                     Git
                                 </p>                     
+                            </li>
+                        </ul>
+                    </div>
+                    <div className='box'>
+                        <ul className='mb-1'>
+                            <li>
+                                <h2>• 리액트와 라이브러리를 이용한 웹 페이지 제작</h2>                   
+                            </li>
+                            <li>                            
+                                <p>
+                                    - api를 이용하여 api 등록, 수정, 삭제 기능 구현
+                                </p>
+                                <p>
+                                    - 차트 라이브러리를 이용하여 차트 구현
+                                </p>   
+                                <p>
+                                    - createContext를 이용하여 웹 페이지 내 데이터 props 관리
+                                </p>
+                                <p>
+                                    - JWT를 이용하여 로그인 구현
+                                </p>
+                            </li>
+                        </ul>
+                        <ul className='mb-1'>
+                            <li>
+                                <h2>• HTML, CSS, J-QUERY를 이용한 웹 사이트 퍼블리싱</h2>                   
+                            </li>
+                            <li>                            
+                                <p>
+                                    -  스마트 팩토리를 이용한 기계 대시보드 퍼블리싱
+                                </p>
+                                <p>
+                                    -  프로그램을 설명하는 웹사이트 퍼블리싱
+                                </p>
+                                <p>
+                                    -  관리자 화면 및 페이지 퍼블리싱
+                                </p>              
+                            </li>
+                        </ul>
+                        <ul className='mb-1'>
+                            <li>
+                                <h2>• 최신 CSS 문법을 사용하여 스크립트 최소화, 직관적인 코드 작성</h2>                   
+                            </li>
+                            <li>                            
+                                <p>
+                                    -  has, Is, container query등을 사용
+                                </p>
+                                <p>
+                                    -  container query를 사용하여 직관적인 코드 작성
+                                </p>           
+                            </li>
+                        </ul>
+                        <ul className='mb-1'>
+                            <li>
+                                <h2>• 크로스 브라우징과 반응형 웹사이트 제작</h2>                   
+                            </li>
+                            <li>                            
+                                <p>
+                                    -  Can i use? 사이트를 이용하여 크로스 브라우징 이슈 확인
+                                </p>
+                                <p>
+                                    -  웹사이트와 태블릿, 모바일을 염두한 반응형 웹사이트 제작
+                                </p>           
+                            </li>
+                        </ul>
+                        <ul className='mb-1'>
+                            <li>
+                                <h2>• Figma와 그림판3D를 이용하여 도면 수정</h2>                   
+                            </li>
+                            <li>                            
+                                <p>
+                                    - 이미지 배경 제거(누끼 따기)
+                                </p>
+                                <p>
+                                    - 잘못 표시된 기계 설비 이미지 수정 등
+                                </p>           
                             </li>
                         </ul>
                     </div>
